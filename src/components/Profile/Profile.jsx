d5014c407fb65313c73a85a598a58ee4fab60cef
Lorena Barrera <l.barreranicolas@gmail.com>
2024-03-02 09:51:01 +0100

    problems

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,11 +4,12 @@ import '../../pages/Register/Register.css'
 
 const Profile = ({ user }) => {
   return (
+    
     <div className="background">
         <div className="profile-container">
             <div className="welcome">
             <div className="image-canva"></div>
-            <h2>¡Hola @{user.data.username}!</h2>
+            <h2>¡Hola @{user?.data?.username}!</h2>
             </div>
         </div>
     </div>
