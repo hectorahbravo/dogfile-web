97f4e20e6b7b7825f521701d81673097a289f274
Lorena Barrera <l.barreranicolas@gmail.com>
2024-03-02 10:28:40 +0100

    test

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { useContext } from "react";
-import AuthContext from "../../contexts/AuthContext";
-import Profile from "../../components/Profile/Profile";
-
-const UserProfile = () => {
-  const { user } = useContext(AuthContext);
-  return <Profile user={user} />;
-};
-
-export default UserProfile;
-=======
 import { useContext } from "react"
 import AuthContext from '../../contexts/AuthContext'
 import Profile from '../../components/Profile/Profile'
@@ -23,4 +11,3 @@ const UserProfile = () => {
 }
 
 export default UserProfile
->>>>>>> 7f7cf5b9c7d3fec31b2900ac7adaa8d8bca36e0b
