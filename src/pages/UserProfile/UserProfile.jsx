5332599208751cebd5e78854619a78e6944a46ac
Hector Hernandez <hectorahbravo@gmail.com>
2024-03-02 10:22:14 +0100

    fixes

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useContext } from "react";
 import AuthContext from "../../contexts/AuthContext";
 import Profile from "../../components/Profile/Profile";
@@ -9,18 +8,3 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
-=======
-import { useContext } from "react"
-import AuthContext from '../../contexts/AuthContext'
-import Profile from '../../components/Profile/Profile'
-
-const UserProfile = () => {
-  const { user } = useContext(AuthContext);
-
-  return (
-    <Profile user={user}/>
-  )
-}
-
-export default UserProfile
->>>>>>> 7f7cf5b9c7d3fec31b2900ac7adaa8d8bca36e0b
