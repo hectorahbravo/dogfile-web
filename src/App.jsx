a8d041778b01edb8b4cf4c0c130322a1de3e75a3
Lorena Barrera <l.barreranicolas@gmail.com>
2024-03-08 14:33:56 +0100

    trying edit profile

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import DogProfilePage from './pages/DogProfilePage/DogProfilePage'
 import { useLocation } from "react-router-dom";
 import EditDog from "./pages/EditDogProfile/EditDogProfile";
 import ReminderForm from "./components/Reminder/ReminderForm";
+import EditProfile from "./pages/EdtiProfile/EditProfile";
 
 function App() {
   const location = useLocation();
@@ -30,15 +31,9 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/:userId/dogs/:dogId" element={<ProtectedRoute><DogProfilePage /></ProtectedRoute>} />
         <Route path="/users/:userId/dogs/:dogId/edit" element={<ProtectedRoute><EditDog /></ProtectedRoute>} />
-        <Route path="/user" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
-        <Route
-          path="/user"
-          element={
-            <ProtectedRoute>
-              <UserProfile />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/users/:userId/edit" element={<ProtectedRoute><EditProfile /></ProtectedRoute>} />
+        <Route path="/user/" element={<ProtectedRoute><UserProfile /></ProtectedRoute>} />
+        <Route path="/user" element={<ProtectedRoute><UserProfile /></ProtectedRoute>}/>
         <Route path="/maps" element={<Map />} />
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/reports/new" element={<Reports />} />
