3720b6034933340250c41b72edbc3a3fdbabd2c4
Hector Hernandez <hectorahbravo@gmail.com>
2024-03-14 20:22:31 +0100

    hector arrechos de contexto y demas

--- src/components/DogProfile/DogProfile.jsx
+++ src/components/DogProfile/DogProfile.jsx
@@ -8,6 +8,15 @@ import { useContext } from "react";
 import AuthContext from "../../contexts/AuthContext";
 import DogContext from "../../contexts/DogContext";
 
+function formatFecha(fecha) {
+  const date = new Date(fecha);
+  const year = date.getFullYear();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+
+  return `${year}-${month}-${day}`;
+}
+
 const DogProfile = () => {
   const { user } = useContext(AuthContext);
   const { fetchDogProfile, dogProfile } = useContext(DogContext);
@@ -28,73 +37,84 @@ const DogProfile = () => {
       });
   }, [userId, dogId, fetchDogProfile]);
 
-  console.log(dogProfile);
-  if (loading) {
+  console.log("profile", dogProfile);
+  /*  if (loading) {
     return <p>Loading...</p>;
-  }
+  } */
 
   return (
-    <div className="background-dogprofile">
-      <div className="dogprofile-container">
-        <div className="avatar-container">
-          <div className="avatar-container-name">
-            <p className="dog-name">{dog.name}</p>
-            <div className="pencil-circle">
-              <Link to={`/users/${user.id}/dogs/${dog.id}/edit`}>
-                <img className="pencil" src={Pencil} alt="editar-perfil" />
-              </Link>
+    <div>
+      {dogProfile === null ? (
+        <p>loading</p>
+      ) : (
+        <div className="background-dogprofile">
+          <div className="dogprofile-container">
+            <div className="avatar-container">
+              <div className="avatar-container-name">
+                <p className="dog-name">{dogProfile.name}</p>
+                <div className="pencil-circle">
+                  <Link to={`/users/${user.id}/dogs/${dogProfile.id}/edit`}>
+                    <img className="pencil" src={Pencil} alt="editar-perfil" />
+                  </Link>
+                </div>
+              </div>
+              <img
+                src={dogProfile.avatar}
+                alt="Dog Avatar"
+                className="avatar"
+              />
             </div>
-          </div>
-          <img src={dogProfile.avatar} alt="Dog Avatar" className="avatar" />
-        </div>
-        <div className="info-container">
-          <div className="info-primer-container">
-            <div className="info-box">
-              <h3>Mis datos</h3>
-              <p>
-                <strong>Peso:</strong> {dog.weight} kg
-              </p>
-              <p>
-                <strong>Fecha de nacimiento:</strong> {dog.birthdate}
-              </p>
-            </div>
-            <div className="info-box">
-              <h3>Mis vacunas</h3>
+            <div className="info-container">
+              <div className="info-primer-container">
+                <div className="info-box">
+                  <h3>Mis datos</h3>
+                  <p>
+                    <strong>Peso:</strong> {dogProfile.weight} kg
+                  </p>
+                  <p>
+                    <strong>Fecha de nacimiento:</strong>{" "}
+                    {formatFecha(dogProfile.birthdate)}
+                  </p>
+                </div>
+                <div className="info-box">
+                  <h3>Mis vacunas</h3>
 
-              <ul>
-                {dog.vaccines.map((vaccine, index) => (
-                  <li key={index}>{vaccine}</li>
-                ))}
-              </ul>
+                  <ul>
+                    {dogProfile.vaccines.map((vaccine, index) => (
+                      <li key={index}>{vaccine}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+              <div className="info-primer-container">
+                <div className="info-box">
+                  <h3>Mi comida</h3>
+                  <p>
+                    <strong>Comida:</strong> {dogProfile.foodType}
+                  </p>
+                  <p>
+                    <strong>Cantidad:</strong> {dogProfile.foodKg} kg
+                  </p>
+                  <p>
+                    <strong>Veces al día:</strong> {dogProfile.foodTimes}
+                  </p>
+                </div>
+                <div className="info-box">
+                  <h3>Mis alergias</h3>
+                  <p> {dogProfile.allergies}</p>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="info-primer-container">
-            <div className="info-box">
-              <h3>Mi comida</h3>
-              <p>
-                <strong>Comida:</strong> {dog.foodType}
-              </p>
-              <p>
-                <strong>Cantidad:</strong> {dog.foodKg} kg
-              </p>
-              <p>
-                <strong>Veces al día:</strong> {dog.foodTimes}
-              </p>
-            </div>
-            <div className="info-box">
-              <h3>Mis alergias</h3>
-              <p> {dog.allergies}</p>
+            <div className="traits-container">
+              <h2>¿Cómo soy?</h2>
+              <div className="traits-box">
+                <h3>Mi carácter</h3>
+                <p>{dogProfile.temperament}</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="traits-container">
-          <h2>¿Cómo soy?</h2>
-          <div className="traits-box">
-            <h3>Mi carácter</h3>
-            <p>{dog.temperament}</p>
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
