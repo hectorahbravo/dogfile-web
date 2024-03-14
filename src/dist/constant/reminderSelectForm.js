c6b085c9cfd177699e7f2b3a601850dd7619a7c6
Hector Hernandez <hectorahbravo@gmail.com>
2024-03-14 20:31:45 +0100

    add dist

---
+++ src/dist/constant/reminderSelectForm.js
@@ -0,0 +1,27 @@
+export const optionsFrecuencia = [
+  { value: "daily", label: "Diariamente" },
+  { value: "monthly", label: "Mensualmente" },
+  { value: "weekly", label: "Semanalmente" },
+  { value: "annually", label: "Anualmente" },
+  { value: "none", label: "None" },
+];
+
+export const optionsIcono = [
+  { value: "icon1", label: "Icono 1" },
+  { value: "icon2", label: "Icono 2" },
+  { value: "icon3", label: "Icono 3" },
+  { value: "icon4", label: "Icono 4" },
+];
+
+export const optionsTipo = [
+  { value: "Medicamentos", label: "Medicamentos" },
+  { value: "Vacuna", label: "Vacuna" },
+  { value: "Baño", label: "Baño" },
+  { value: "Peluqueria", label: "Peluqueria" },
+  { value: "Visita al veterinario", label: "Visita al veterinario" },
+  { value: "Pastillas antiparacitarias", label: "Pastillas antiparacitarias" },
+  { value: "Pipeta antiparacitaria", label: "Pipeta antiparacitaria" },
+  { value: "Collar antiparacitario", label: "Collar antiparacitario" },
+  { value: "Vacuna polivalente rabia", label: "Vacuna polivalente rabia" },
+  { value: "Otros", label: "Otros" },
+];
