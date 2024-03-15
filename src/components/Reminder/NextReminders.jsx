78c705bf45890dea90be0e627b2dff7786f523e0
Hector Hernandez <hectorahbravo@gmail.com>
2024-03-15 18:30:43 +0100

    fixes

--- src/components/Reminder/NextReminders.jsx
+++ src/components/Reminder/NextReminders.jsx
@@ -1,40 +1,14 @@
+import React from "react";
+
 function NextReminders({ reminders }) {
   const currentDate = new Date();
   const upcomingReminders = reminders
     ? reminders
         .filter((reminder) => {
-          if (!reminder.endDate || new Date(reminder.endDate) >= currentDate) {
-            // Si el recordatorio se repite
-            if (reminder.repeat) {
-              switch (reminder.frequency) {
-                case "daily":
-                  return true;
-                case "weekly":
-                  const startWeek = new Date(reminder.startDate).getDay();
-                  const currentWeek = currentDate.getDay();
-                  const daysDiff =
-                    (currentDate - new Date(reminder.startDate)) /
-                    (1000 * 60 * 60 * 24);
-                  return startWeek === currentWeek && daysDiff % 7 === 0;
-                case "monthly":
-                  return (
-                    currentDate.getDate() ===
-                    new Date(reminder.startDate).getDate()
-                  );
-                case "annually":
-                  const reminderStartDate = new Date(reminder.startDate);
-                  return (
-                    currentDate.getDate() === reminderStartDate.getDate() &&
-                    currentDate.getMonth() === reminderStartDate.getMonth()
-                  );
-                default:
-                  return false;
-              }
-            } else {
-              return new Date(reminder.startDate) >= currentDate;
-            }
-          }
-          return false;
+          return (
+            (!reminder.endDate || new Date(reminder.endDate) >= currentDate) &&
+            new Date(reminder.startDate) >= currentDate
+          );
         })
         .sort((a, b) => new Date(a.startDate) - new Date(b.startDate))
     : [];
